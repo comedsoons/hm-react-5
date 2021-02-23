5387132d245ee472a9ce4363dd3463a06b018039
comedsoons <67965566+comedsoons@users.noreply.github.com>
2021-02-23 16:40:20 +0200

    hm5

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Menu = ({firstElement, secondElement, thirdElement, link}) => (
+  <nav>
+    <ul>
+      <li>{firstElement}</li>
+      <li>{secondElement}</li>
+      <li>
+        <a href={link}>{thirdElement}</a>
+      </li>
+    </ul>
+  </nav>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render (
+<Menu
+firstElement="Перший елемент списку"
+secondElement="Другий елемент списку"
+thirdElement="Google"
+link="https://www.google.com.ua/"
+/>,
+document.getElementById("root")
+);
